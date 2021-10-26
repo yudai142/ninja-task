0f00a256c99bbe3bc708e373ade576d374657d29
yudai142 <yudai.shimoji.42865233@gmail.com>
2021-10-27 00:16:18 +0900

    ブレイクポイントとビューポート

---
+++ src/views/Breakpoints&Visibility.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>This is the homepage</h1>
+    <v-divider></v-divider>
+
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+
+    <v-btn class="hidden-sm-only">click me</v-btn>
+
+  </div>
+</template>
---
+++ src/views/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>This is the homepage</h1>
+    <v-divider></v-divider>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn flat color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab dark small depressed color="purple">
+      <v-icon dark>favorite</v-icon>
+    </v-btn>
+  </div>
+</template>
